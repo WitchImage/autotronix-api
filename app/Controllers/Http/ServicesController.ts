d680b8a55cd98a5ad49d0f89f952d473288b0d2c
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-04-28 18:19:33 -0500

    feat: add index for services

--- app/Controllers/Http/ServicesController.ts
+++ app/Controllers/Http/ServicesController.ts
@@ -1,3 +1,9 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Service from 'App/Models/Service';
 
-export default class ServicesController {}
+export default class ServicesController {
+    async index({ response }: HttpContextContract) {
+        const services = await Service.all();
+        return response.ok(services);
+    }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -20,3 +20,4 @@
 
 import './routes/auth.routes';
 import './routes/client.routes';
+import './routes/service.routes';
