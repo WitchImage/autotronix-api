5e85a1dc5c4c78bf6788813d03a6bdc8531d605d
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-05-11 17:43:08 -0500

    feat: add update client endpoint

--- app/Controllers/Http/ClientsController.ts
+++ app/Controllers/Http/ClientsController.ts
@@ -1,5 +1,6 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import Client from 'App/Models/Client';
+import User from 'App/Models/User';
 
 export default class ClientsController {
     async show({ response, params: { id } }: HttpContextContract) {
@@ -20,5 +21,36 @@ export default class ClientsController {
         const client = await Client.query().where('user_id', userId).preload('user').firstOrFail();
 
         return response.ok(client);
+    }
+
+    async update({ request, response, params: { clientId } }: HttpContextContract) {
+        const { name, surname, address, phone, email } = request.body() as unknown as {
+            name: string;
+            surname: string;
+            address: string;
+            phone: string;
+            email: string;
+        };
+
+        const client = await Client.findOrFail(clientId);
+        const user = await User.findOrFail(client.userId);
+
+        await client
+            .merge({
+                name,
+                surname,
+                address,
+                phone,
+            })
+            .save();
+
+        await user.merge({ email }).save();
+
+        const clientUpdated = await Client.query()
+            .where('id', clientId)
+            .preload('user')
+            .firstOrFail();
+
+        return response.ok(clientUpdated);
     }
 }
--- start/routes/client.routes.ts
+++ start/routes/client.routes.ts
@@ -2,6 +2,7 @@ import Route from '@ioc:Adonis/Core/Route';
 
 Route.group(() => {
     Route.get('/:id', 'ClientsController.show');
+    Route.put('/:clientId', 'ClientsController.update').middleware('auth');
     Route.get('/document/:document', 'ClientsController.getByDocument');
     Route.get('/profile/:userId', 'ClientsController.getByUserId').middleware('auth');
 }).prefix('clients');
