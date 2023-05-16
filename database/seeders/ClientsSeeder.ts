cbbf6885c71ea451cb7161c1852197619cb519a3
Simón Felipe Muñoz Osorio <simonfelipemunozosorio@gmail.com>
2023-05-16 15:22:01 -0500

    feat: add Vechicles seed to ClientsSeeder

--- database/seeders/ClientsSeeder.ts
+++ database/seeders/ClientsSeeder.ts
@@ -1,5 +1,6 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
 import Client from 'App/Models/Client';
+import Vehicle from 'App/Models/Vehicle';
 
 export default class extends BaseSeeder {
     public async run() {
@@ -27,6 +28,36 @@ export default class extends BaseSeeder {
                 phone: '573132143975',
                 address: 'Avenida siempre viva',
                 document: '7777',
+            },
+        ]);
+
+        await Vehicle.updateOrCreateMany('id', [
+            {
+                id: 1,
+                clientId: 1,
+                color: 'black',
+                description: 'My description',
+                model: 'Chevrolet Tracker',
+                plate: 'XXX-666',
+                year: '2018',
+            },
+            {
+                id: 1,
+                clientId: 1,
+                color: 'white',
+                description: 'My description',
+                model: 'Renault Duster',
+                plate: 'YYY-555',
+                year: '2013',
+            },
+            {
+                id: 2,
+                clientId: 1,
+                color: 'red',
+                description: 'My description',
+                model: 'Mazda 3',
+                plate: 'WWW-444',
+                year: '2017',
             },
         ]);
     }
